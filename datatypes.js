93e201204822573fdd8dbeba00a8c2d29f057aca
iamVenky7 <venkateshpamidi7@gmail.com>
2024-01-22 00:09:10 +0530

    DataTypes in JS

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+// data types in JS
+// PRIMITIVE -  SINGLE VALUE | SIMPLE , DONT DEPEND ON OTHER DATA TYPES
+var name = "Venkatesh"; //string, incl 'STRING ' , `STRING `
+var age = 23; //number  incl 2.3 , 5.33, 1000
+var isMarried = false; //boolean- true/false
+var company; // not assigneg any value
+var marks = undefined; // undefined - defined value as undefined
+var score = null; //null - empty value
+
+console.log(name);
+console.log(age);
+console.log(isMarried);
+console.log(company);
+console.log(marks);
+console.log(score);
+
+//NON-PRIMITIVE  -- COMPOSITION OF OTHER DATATYPES | COMPLEX DATATYPE
+// Object -  Collection of key:value pairs
+var product = {
+  color: "Blue",
+  price: 1200,
+};
+
+console.log(product);
---
+++ displayData.js
@@ -0,0 +1,7 @@
+console.log("Start");
+let firstName = "Venkatesh";
+let lastName = "Pamidi";
+console.log(firstName, lastName);
+var age = 23;
+console.log(age);
+console.log("End");
