f2db70e1a767f38bf3de758f2e56e6484daf1a34
iamVenky7 <venkateshpamidi7@gmail.com>
2024-01-21 23:10:10 +0530

    variable declarations

---
+++ variables.js
@@ -0,0 +1,13 @@
+// 1. using var keyword
+
+var firstName = "Venkatesh";
+var lastName = "Pamidi";
+
+// 2.using let keyword
+
+let marks = 90;
+let isMarried = false;
+
+// 3.using const keyword
+const age = 24;
+const city = "Hyderabad";
---
+++ wrongSyntaxvariables.js
@@ -0,0 +1,4 @@
+const 0name='Venkatesh'; // cant start with number
+const var = 'name';  // cant use keywords for variable naming
+const @name = 'Venky';  //cant start with special char
+const venky = 'venkatesh' //no meaning full variable name
