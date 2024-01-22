4907cfdfa30de75efd8d23b782b92bd7b45b073c
iamVenky7 <venkateshpamidi7@gmail.com>
2024-01-22 07:36:40 +0530

    splChars and comments

---
+++ comments.js
@@ -0,0 +1,7 @@
+// console.log("Hello world");
+//printing hello world
+
+/* this is a multiline comment
+console.log("Start");
+console.log("End");
+*/
---
+++ splChars.js
@@ -0,0 +1,8 @@
+let fullName = "Venkatesh \t Pamidi";
+console.log(fullName);
+let sentence = "This is \\n new line";
+console.log(sentence);
+
+//print with \n in sentence
+let sentence1 = "This is \\n new line";
+console.log(sentence);
